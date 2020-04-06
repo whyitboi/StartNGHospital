2c42cdb2262e708caa1244f4688d77b4d0aabce8
whyitboi <poisamoje@gmail>
2020-04-06 22:21:56 +0100

    second commit from vscode

--- processingRegister.php
+++ processingRegister.php
@@ -5,13 +5,23 @@ print_r($_POST);
 //collecting the data
 $first_name =$_POST["first_name"];
 $last_name =$_POST["last_name"];
-$first_name =$_POST["email"];
-$first_name =$_POST["password"];
-$first_name =$_POST["designation"];
-$first_name =$_POST["department"];
-$first_name =$_POST["first_name"];
+$email =$_POST["email"];
+$password =$_POST["password"];
+$designation =$_POST["designation"];
+$department =$_POST["department"];
+
+$errorArray =[];
 
 //Validating the data
+if($first_name == ""){
+    $errorArray = "First name cannot be blank";
+}
+ if($last_name == ""){
+    $errorArray = "Last name cannot be blank";
+}
+
+print_r($errorArray);
+
 
 //Saving data to file
 
